1e96f01c3b5bea9cc63df25f8d42a35a9b9875c3
LuckerDeveloper <61561232+LuckerDeveloper@users.noreply.github.com>
2023-11-12 22:53:18 +0300

    migrate plugins to libs approach

--- requestyesno/build.gradle.kts
+++ requestyesno/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
     id(libs.plugins.kotlin.kapt.get().pluginId)
-    id(libs.plugins.plugin.serialization.get().pluginId)
+    id(libs.plugins.kotlin.plugin.serialization.get().pluginId)
 }
 
 android {
